6a115c0a87143ff163c9c16aff075acb8bbc3de6
jefin <techy.jefinjoji@gmail.com>
2025-05-25 12:42:12 +0530

    Refactor rock-paper-scissors code for optimization and clarity

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -2,14 +2,11 @@ function playGame(){
     let computerScore = 0;
     let userScore = 0;
     let totalPoints = 5;
-    choiceDisplay.textContent = "";
-    statDisplay.textContent = "";
-    resultDisplay.textContent = "";
+    choiceDisplay.textContent = ``;
+    statDisplay.textContent = ``;
+    resultDisplay.textContent = ``;
     userScoreDisplay.textContent = userScore;
     computerScoreDisplay.textContent = computerScore;
-    const rockBtn = document.querySelector("#rock-btn");
-    const paperBtn = document.querySelector("#paper-btn");
-    const scissorsBtn = document.querySelector("#scissors-btn");
 
     function getComputerChoice(){
         let randomValue = Math.random();
@@ -25,60 +22,61 @@ function playGame(){
     function playRound(userChoice, computerChoice){
         choiceDisplay.textContent = `You chooses : ${userChoice.toLowerCase()} | Computer chooses : ${computerChoice.toLowerCase()}`;
         if(userChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "rock"){
-            statDisplay.textContent = "No points";
+            statDisplay.textContent = `No points`;
         }
         else if(userChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "paper"){
-            statDisplay.textContent = "Computer scores";
+            statDisplay.textContent = `Computer scores`;
             computerScore++;
         }
         else if(userChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissors"){
-            statDisplay.textContent = "You scores";
+            statDisplay.textContent = `You scores`;
             userScore++;
         }
         else if(userChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock"){
-            statDisplay.textContent = "You scores";
+            statDisplay.textContent = `You scores`;
             userScore++;
         }
         else if(userChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "paper"){
-            statDisplay.textContent = "No points";
+            statDisplay.textContent = `No points`;
         }
         else if(userChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "scissors"){
-            statDisplay.textContent = "Computer scores";
+            statDisplay.textContent = `Computer scores`;
             computerScore++;
         }
         else if(userChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "rock"){
-            statDisplay.textContent = "Computer scores";
+            statDisplay.textContent = `Computer scores`;
             computerScore++;
         }
         else if(userChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "paper"){
-            statDisplay.textContent = "You scores";
+            statDisplay.textContent = `You scores`;
             userScore++;
         }
         else if(userChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "scissors"){
-            statDisplay.textContent = "No points";
+            statDisplay.textContent = `No points`;
         }
         userScoreDisplay.textContent = userScore;
         computerScoreDisplay.textContent = computerScore;
-        if(userScore == totalPoints || computerScore == totalPoints){
-            if(userScore > computerScore){
-                resultDisplay.textContent = "You won";
-            }
-            else{
-                resultDisplay.textContent = "Computer wins";
-            }
+        if(userScore >= totalPoints || computerScore >= totalPoints){
             rockBtn.removeEventListener('click', rockHandler);
             paperBtn.removeEventListener('click', paperHandler);
             scissorsBtn.removeEventListener('click', scissorsHandler);
-            document.querySelector("#total-points").focus();
-            return;
+            instruction1Display.textContent = ``;
+            instruction2Display.textContent = ``;
+            if(userScore > computerScore){
+                resultDisplay.textContent = `You won`;
+            }
+            else{
+                resultDisplay.textContent = `Computer wins`;
+            }
+            inputTotalPoints.focus();
         }
     }
 
-    if(Number(document.querySelector("#total-points").value)){
-        totalPoints = Number(document.querySelector("#total-points").value);
+    if(Number(inputTotalPoints.value)){
+        totalPoints = Number(inputTotalPoints.value);
     }
 
-    document.querySelector("#total-points").value = "";
+    inputTotalPoints.value = ``;
 
     instruction1Display.textContent = `Game started for ${totalPoints} points`;
     instruction2Display.textContent = `Select you choice`;
@@ -92,13 +90,17 @@ function playGame(){
     scissorsBtn.addEventListener('click', scissorsHandler);
 }
 
+const inputTotalPoints = document.querySelector("#total-points");
 const instruction1Display = document.querySelector("#instruction1");
 const instruction2Display = document.querySelector("#instruction2");
+const rockBtn = document.querySelector("#rock-btn");
+const paperBtn = document.querySelector("#paper-btn");
+const scissorsBtn = document.querySelector("#scissors-btn");
 const choiceDisplay = document.querySelector("#choices");
 const statDisplay = document.querySelector("#stats");
 const userScoreDisplay = document.querySelector("#user-score");
 const computerScoreDisplay = document.querySelector("#computer-score");
 const resultDisplay = document.querySelector("#result");
 const startBtn = document.querySelector("#start");
-document.querySelector("#total-points").focus();
+inputTotalPoints.focus();
 startBtn.addEventListener('click', playGame);
