f77f05474bd643c5c2aa3d7fd96b0e0d260ace6f
jefin <techy.jefinjoji@gmail.com>
2025-06-08 02:16:18 +0530

    etch-a-sketch/: add custom sketch pad generator

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,15 +1,31 @@
-const container = document.querySelector('#container');
+function generateCustomPlayground(numOfRows, numOfColumns){
+    container.innerHTML = '';
+    boxWidth = 640 / numOfColumns;
+    boxHeight = 640 / numOfRows;
+    for(let i = 1; i <= numOfRows; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for(let j = 1; j <= numOfColumns; j++){
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.style.width = boxWidth + 'px';
+            box.style.height = boxHeight + 'px';
+            box.addEventListener('mouseenter', (event) => {
+                event.target.style.backgroundColor = 'black';
+            })
+            row.appendChild(box);
+        }
+        container.appendChild(row);
+    };
+}
 
-for(let i = 1; i <= 16; i++){
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for(let j = 1; j <= 16; j++){
-        const box = document.createElement('div');
-        box.classList.add('box');
-        box.addEventListener('mouseenter', (event) => {
-            event.target.style.backgroundColor = 'black';
-        })
-        row.appendChild(box);
-    }
-    container.appendChild(row);
-};
+const container = document.querySelector('#container');
+const generateCustomPlaygroundButton = document.querySelector('#generate-custom-playground-button')
+
+generateCustomPlayground(16, 16);
+
+generateCustomPlaygroundButton.addEventListener('click', () => {
+    noOfRows = Number(prompt("Enter no of rows"));
+    noOfColumns = Number(prompt("Enter no of columns"));
+    generateCustomPlayground(noOfRows, noOfColumns);
+})
